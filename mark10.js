e7dc2c2a8b0051938f16e634cb94eb27ef40b4c5
utkarshducoder <83503804+utkarshducoder@users.noreply.github.com>
2021-08-28 15:12:06 +0530

    Add files via upload

---
+++ mark10.js
@@ -0,0 +1,54 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-btn");
+const errorMessage = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const avlblNotes = [2000, 500, 100, 20, 5, 1];
+
+
+
+function validAmount(){
+    hideMessage();
+    if(billAmount.value > 0){
+
+        if(cashGiven.value > billAmount.value){
+            const amountToReturn = cashGiven.value - billAmount.value;
+            calculate(amountToReturn);
+
+        }
+        else{
+           showMessage("The cash provided should atleast be equal to the bill amount");
+            //errorMessage.innerText = "Cash provided must be equal to the bill amount";
+
+        }
+
+    }
+    else{
+        showMessage("Invalid Bill Amount");
+        //errorMessage.innerText = "invalid Bill amount";
+       // show("Invalid Bill Amount");
+       
+       
+    }
+}
+
+function calculate(amountToReturn){
+    for( let i = 0; i < avlblNotes.length; i++){
+    
+        const numberOfNotes = Math.trunc(amountToReturn / avlblNotes[i]);
+        amountToReturn %= avlblNotes[i];
+        noOfNotes[i].innerText = numberOfNotes; 
+    }
+
+}
+
+function hideMessage(){
+    errorMessage.style.display = "none";
+}
+
+function showMessage(msg){
+       errorMessage.style.display = "block";  //When we want to show the mssge we set it to block so we can see the message
+       errorMessage.innerText = msg;
+}
+
+checkButton.addEventListener("click", validAmount);
